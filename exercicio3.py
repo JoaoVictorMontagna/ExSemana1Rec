b646ea94434cc697af38729eb362596329ce98fa
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2023-06-01 13:00:05 -0300

    Update

--- exercicio3.py
+++ exercicio3.py
@@ -5,9 +5,17 @@
 
 
 # Crie a função que será avaliada no exercício aqui
-def soma_dos_aninhados(lista):
-    soma=200+31
+def soma_dos_aninhados(lista_aninhada):
+    soma = 0
+    for lista in lista_aninhada:
+        soma += sum(lista)
     return soma
+
+lista = [[11, 22], [33], [44, 55, 66]]
+outra_lista = [[1, 2, 3, 4], [3, 3], [4, 6]]
+
+print(soma_dos_aninhados(lista))  # Output: 231
+print(soma_dos_aninhados(outra_lista))  # Output: 23
 
 
 
